699337390a7132858fad6bf74af7f5aa9f6a9848
dev <stavinsky@gmail.com>
2024-05-03 13:31:39 +0400

    internal uart, build script for gw_sh

--- scripts/build.tcl
+++ scripts/build.tcl
@@ -1,16 +1,21 @@
-add_file top.v
-add_file constants.txt
 add_file pins.cst
-add_file queue.v
-add_file rpll.v
-add_file uart_tx.v
-add_file usb_pll.v
+
+add_file modules/top.v
+add_file modules/constants.txt
+
+add_file modules/queue.v
+add_file modules/rpll.v
+add_file modules/uart_tx.v
+add_file modules/usb_pll.v
+add_file modules/buffered_uart_tx.v
+add_file modules/power_on_reset.v
+add_file modules/buffered_usb.v
 add_file usbcorev/usb.v
 add_file usbcorev/usb_recv.v
 add_file usbcorev/usb_tx.v
 add_file usbcorev/usb_utils.v
 add_file usbcorev/utils.v
-add_file buffered_uart_tx.v
-set_device GW1NR-LV9QN88PC6/I5
+
+set_device GW1NR-LV9QN88PC6/I5 -device_version C
 set_option -top_module  top
 run all
